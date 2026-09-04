528057c6860f4e24200cc0bd7caf1a6b61c46bf2
rynverse <nmagno@proton.me>
2026-09-04 13:33:32 +0100

    Added finding IP from URL Functionality

--- Java Port Scanner/src/Main.java
+++ Java Port Scanner/src/Main.java
@@ -6,15 +6,21 @@ public class Main{
     public static void main(String[] args){
         // Creates a new input variable of type Scanner
         Scanner input = new Scanner(System.in);
-        String address;
+        String host;
         final int startPort = 1;
         final int endPort = 1024;
 
         System.out.println("Enter IP Address to have ports scanned\n");
 
         // Takes the input of the next line
-        address = input.nextLine();
+        try{
+            host = input.nextLine().trim();
+            InetAddress address = InetAddress.getByName(host);
+            System.out.println(host);
+        } catch (UnknownHostException e) {
+            System.out.println("Could not Resolve host..");
+        }
 
-        System.out.println(address);
+
     }
 }
